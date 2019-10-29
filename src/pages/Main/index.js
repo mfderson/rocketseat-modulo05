5c0ff36752325241396b9963f0d06e9042f2502b
Anderson <mfderson@gmail.com>
2019-10-28 21:36:23 -0300

    Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGitAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Hello world!!!</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGitAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disable>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,45 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #fff;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto; /* margin 80px top e bottom e automatica right e left */
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row; /* Itens dispostos em linha */
+    align-items: center; /* Itens acima centralizados verticalmente */
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1; /* Ocupe toda a largura */
+    border: 1px solid #eee; /* borda de 1px */
+    padding: 10px 15px; /* 10px top e bottom e 15px right left */
+    border-radius: 4px; /* arredondamento das bordas */
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
